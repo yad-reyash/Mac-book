9b99912dc826c4dc129073b2a1c9aada1712b090
yad-Reyansh <reyansh7004.com>
2025-11-16 22:25:52 +0545

    Fix: accessibility and GSAP refactor

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
+
+gsap.registerPlugin(ScrollTrigger)
+
+const App = () => {
+    return (
+        <main>
+            <NavBar />
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
+        </main>
+    )
+}
+
+export default App
